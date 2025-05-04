855ef5be256798769fce0f01009ad87dd9d1ae0a
Saptarshi Mondal <saptarshimondal.mail@gmail.com>
2025-05-04 21:27:50 +0530

    firestore database created and functional

---
+++ src/dashboard.js
@@ -0,0 +1,23 @@
+//onAuthStateChanged
+
+onAuthStateChanged(auth, (user) => {
+    if(!user) {
+
+      window.location.href = '/index.html';
+
+    }
+  })
+  //SignOut function
+  logout.addEventListener('click', () => {
+    
+
+    signOut(auth).then(() => {
+      // Sign-out successful.
+      alert('You are Signed Out.');
+      window.location.href = '/index.html';
+    }).catch((error) => {
+      alert('An error occured:'+ error.Message);
+      // An error happened.
+    })
+
+  });
--- src/main.js
+++ src/main.js
@@ -1,12 +1,11 @@
 import './style.scss';
 import { initializeApp } from "firebase/app";
-import { getAuth, 
-         signInWithEmailAndPassword, 
-         createUserWithEmailAndPassword } from "firebase/auth";
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword, 
+         onAuthStateChanged, signOut } from "firebase/auth";
+import { getFirestore, doc, setDoc } from 'firebase/firestore';
 
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: "AIzaSyC3UhFNEGeyMNE5zsb4e4Y-TGuJxy8ZEfw",
   authDomain: "gassys-5b3df.firebaseapp.com",
@@ -16,16 +15,24 @@ const firebaseConfig = {
   appId: "1:658508671421:web:06a05d2d53257181c64ad4"
 };
 
+const emailId = document.getElementById("userEmail");
+const passwordInput = document.getElementById("userPassword");
+const loginBtn = document.getElementById("btnLogin");
+const createAccount = document.getElementById("btnCreate");
+const newName = document.getElementById("newName");
+const emailNew = document.getElementById("newEmail");
+const passwordNew = document.getElementById("newPassword");
+const statusMsg = document.getElementById('status');
+const logout = document.getElementById('logout');
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
-const auth = getAuth();
 
 
-  const emailId = document.getElementById("userEmail");
-  const passwordInput = document.getElementById("userPassword");
-  const loginBtn = document.getElementById("btnLogin");
-
+//login function using email and password.
     loginBtn.addEventListener('click', () => {
       const email = emailId.value;
       const password = passwordInput.value;  
@@ -43,29 +50,53 @@ const auth = getAuth();
 
       })
         
-    });
-    
-    const createAccount = document.getElementById("btnCreate");
-    const emailNew = document.getElementById("newEmail");
-    const passwordNew = document.getElementById("newPassword");
+    });  
 
-      createAccount.addEventListener('click', () => {
+      //Registration using email and password and storing data to firestore.
+      createAccount.addEventListener('click', async () => {
       
       const email = emailNew.value;
       const password = passwordNew.value; 
-      createUserWithEmailAndPassword(auth, email, password)
+      await createUserWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
     // Signed up 
-    const user = userCredential.user;
-    alert("Account Created.");
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // ..
-  });
+      const user = userCredential.user; 
+    
+    //adding new data
+    try {
 
-})
+      const docRef = doc(db, "users", userCredential.user.uid);
+      setDoc(docRef, {
+        name: newName.value,
+        email:emailNew.value
+      }); 
+          /* alert("Account Created."); */
+          statusMsg.innerHTML = "Account Created Successfully.";
+          newName.value = "";
+          emailNew.value = "";
+          passwordNew.value = "";
+          confirmPwd.value = "";
+
+    } catch (e) {
+
+      console.error("Error adding document: ", e);
+
+    }
+  
+    })
+  
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage);
+      // ..
+    });   
+  })
+  
+  
+
+
+
 
 
 
